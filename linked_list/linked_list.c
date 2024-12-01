028ee7a8c601966cb7fab5a6e538d2a5054ee0bf
jarobeteg <jaroslavtm@gmail.com>
2024-12-01 21:37:09 +0100

    fixed a naming error and added get first, get last, get element

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -62,17 +62,32 @@ void linked_list_remove_element(LinkedList *linked_list, const void *element) {
 }
 
 void *linked_list_get_element(LinkedList *linked_list, size_t index) {
+    if (index >= linked_list->size) {
+        printf("%sout of bounds%s\n", RED, RESET);
+        exit(EXIT_FAILURE);
+    }
 
+    LinkedList *current = linked_list;
+    size_t current_index = 0;
+    while (current_index < index) {
+        current = current->next;
+        current_index++;
+    }
+
+    return current->data;
 }
 
 void *linked_list_get_first(LinkedList *linked_list) {
-    LinkedList *current = linked_list;
-
+    return linked_list->data;
 }
 
 void *linked_list_get_last(LinkedList *linked_list) {
-    LinkedList *current = linked_list;
+    LinkedList *last = linked_list;
+    while (last->next != NULL) {
+        last = last->next;
+    }
 
+    return last->data;
 }
 
 void linked_list_free(LinkedList *linked_list) {
