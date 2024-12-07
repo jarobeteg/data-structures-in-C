b3191d305956e45d8c9f36267189d415d97af795
jarobeteg <jaroslavtm@gmail.com>
2024-12-07 20:26:41 +0100

    added enqueue and dequeue function for queue data structures

--- queue/queue.c
+++ queue/queue.c
@@ -15,6 +15,17 @@ void queue_init(Queue *queue, size_t element_size, size_t capacity) {
     queue->array = array;
 }
 
+void queue_enqueue(Queue *queue, const void *element) {
+    array_add_element(&queue->array, element);
+    queue->size++;
+}
+
+void queue_dequeue(Queue *queue) {
+    const void *element = array_get_element(&queue->array, 0);
+    array_remove_element(&queue->array, element);
+    queue->size--;
+}
+
 void queue_free(Queue *queue) {
     array_free(&queue->array);
     queue->size = -1;
--- queue/queue.h
+++ queue/queue.h
@@ -11,8 +11,8 @@ typedef struct Queue {
 } Queue;
 
 void queue_init(Queue *queue, size_t element_size, size_t capacity);
-void enqueue(Queue *queue, const void *element);
-void dequeue(Queue *queue);
+void queue_enqueue(Queue *queue, const void *element);
+void queue_dequeue(Queue *queue);
 void *queue_front(Queue *queue);
 void *queue_rear(Queue *queue);
 bool is_queue_empty(Queue *queue);
