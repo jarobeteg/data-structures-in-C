dc19b08c2e57a77fef86d096a2503c76083be22c
jarobeteg <jaroslavtm@gmail.com>
2024-12-14 20:08:56 +0100

    deque data structure front, rear and is empty functions added

--- deque/deque.c
+++ deque/deque.c
@@ -37,6 +37,20 @@ void deque_pop_rear(Deque *deque) {
     deque->size--;
 }
 
+void *deque_front(Deque *deque) {
+    const void *element = array_get_element(&deque->array, 0);
+    return (char *)element;
+}
+
+void *deque_rear(Deque *deque) {
+    const void *element = array_get_element(&deque->array, deque->size);
+    return (char *)element;
+}
+
+bool is_deque_empty(Deque *deque) {
+    return deque->size == -1;
+}
+
 void deque_free(Deque *deque) {
     array_free(&deque->array);
     deque->size = -1;
